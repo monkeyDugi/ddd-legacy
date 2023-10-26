5e43979fb42f45a4d80d30e1a3f554748066d0ef
monkeydugi <lbd4946@gmail.com>
2023-10-26 13:58:35 +0900

    etc: 주문 생성 코드에 주석 추가

--- src/main/java/kitchenpos/application/OrderService.java
+++ src/main/java/kitchenpos/application/OrderService.java
@@ -32,34 +32,49 @@ public class OrderService {
     @Transactional
     public Order create(final Order request) {
         final OrderType type = request.getType();
+        // 타입 필수
         if (Objects.isNull(type)) {
             throw new IllegalArgumentException();
         }
+
+        // 주문 메뉴 필수
         final List<OrderLineItem> orderLineItemRequests = request.getOrderLineItems();
         if (Objects.isNull(orderLineItemRequests) || orderLineItemRequests.isEmpty()) {
             throw new IllegalArgumentException();
         }
+
+        // 주문 메뉴 찾기
         final List<Menu> menus = menuRepository.findAllByIdIn(
             orderLineItemRequests.stream()
                 .map(OrderLineItem::getMenuId)
                 .collect(Collectors.toList())
         );
+
+        // 주문 메뉴가 실제 메뉴에 존재하지 않으면 주문 불가
         if (menus.size() != orderLineItemRequests.size()) {
             throw new IllegalArgumentException();
         }
+
+
         final List<OrderLineItem> orderLineItems = new ArrayList<>();
+        // 주문은 여러 메뉴가 가능하기 때문에 루프
         for (final OrderLineItem orderLineItemRequest : orderLineItemRequests) {
             final long quantity = orderLineItemRequest.getQuantity();
+            // 먹고갈 경우 주문 메뉴 중 1개라도 0개 미만이면 안된다. 버그이다. 1개 미만으로 바껴야 한다.
             if (type != OrderType.EAT_IN) {
                 if (quantity < 0) {
                     throw new IllegalArgumentException();
                 }
             }
+
+            // 존재하지 않는 메뉴 주문 불가
             final Menu menu = menuRepository.findById(orderLineItemRequest.getMenuId())
                 .orElseThrow(NoSuchElementException::new);
+            // 비활성된 메뉴 주문 불가
             if (!menu.isDisplayed()) {
                 throw new IllegalStateException();
             }
+            // 메뉴의 가격과 주문 메뉴의 가격이 다르면 주문 불가
             if (menu.getPrice().compareTo(orderLineItemRequest.getPrice()) != 0) {
                 throw new IllegalArgumentException();
             }
@@ -68,20 +83,27 @@ public class OrderService {
             orderLineItem.setQuantity(quantity);
             orderLineItems.add(orderLineItem);
         }
+
+        // 주문 대기 중이르 초기 셋팅
         Order order = new Order();
         order.setId(UUID.randomUUID());
         order.setType(type);
         order.setStatus(OrderStatus.WAITING);
         order.setOrderDateTime(LocalDateTime.now());
         order.setOrderLineItems(orderLineItems);
+
+        // 배달일 경우
         if (type == OrderType.DELIVERY) {
             final String deliveryAddress = request.getDeliveryAddress();
+            // 주소 필수
             if (Objects.isNull(deliveryAddress) || deliveryAddress.isEmpty()) {
                 throw new IllegalArgumentException();
             }
             order.setDeliveryAddress(deliveryAddress);
         }
+
         if (type == OrderType.EAT_IN) {
+            // 먹고가려면 비어있는 테이블이 있어야 한다.
             final OrderTable orderTable = orderTableRepository.findById(request.getOrderTableId())
                 .orElseThrow(NoSuchElementException::new);
             if (!orderTable.isOccupied()) {
