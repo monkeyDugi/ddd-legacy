8f669d9a7985a61696c3b843ff5abbed268a39c2
monkeyDugi <lbd4946@gmail.com>
2023-11-01 18:35:43 +0900

    test: 상품 가격 수정 시 메뉴 상품 가격이 메뉴 가격보다 작아지면 해당 메뉴는 비활성화되는지 테스트 코드 작성

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,8 +1,14 @@
 package kitchenpos.application;
 
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.MenuProductRepository;
 import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
 import kitchenpos.infra.PurgomalumClient;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -19,6 +25,8 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -35,6 +43,12 @@ class ProductServiceTest {
 
     @Autowired
     private MenuRepository menuRepository;
+
+    @Autowired
+    private MenuGroupRepository menuGroupRepository;
+
+    @Autowired
+    private MenuProductRepository menuProductRepository;
 
     @MockBean
     private PurgomalumClient purgomalumClient;
@@ -89,14 +103,30 @@ class ProductServiceTest {
             assertThat(actualProduct.getName()).isEqualTo("강정치킨");
         }
 
-        @DisplayName("메뉴에 등록된 상품일 경우 변경된 상품 가격이 메뉴 상품의 합보다 작으면 해당 메뉴는 비활성화 된다.")
+        @DisplayName("메뉴에 등록된 상품일 경우 메뉴 가격이 메뉴 상품의 합보다 작으면 해당 메뉴는 비활성화 된다.")
         @Test
         void 상품_가격_변경하기_메뉴에_등록된_상품() {
             // given
+            Product product = new Product();
+            product.setPrice(BigDecimal.valueOf(17_000));
+            product.setName("강정치킨");
+            given(purgomalumClient.containsProfanity("강정치킨")).willReturn(Boolean.FALSE);
+            Product createProduct = sut.create(product);
+
+            MenuGroup menuGroup = createMenuGroup("한마리메뉴");
+            Menu menu = createMenu(createProduct, menuGroup, "후라이드치킨", 17_000, true);
+
+            Product changeProduct = new Product();
+            changeProduct.setPrice(BigDecimal.valueOf(10_000));
 
             // when
+            Product actualProduct = sut.changePrice(createProduct.getId(), changeProduct);
 
             // then
+            assertThat(actualProduct.getPrice()).isEqualTo(BigDecimal.valueOf(10_000));
+
+            Menu findMenu = menuRepository.findById(menu.getId()).get();
+            assertThat(findMenu.isDisplayed()).isFalse();
         }
     }
 
@@ -151,5 +181,31 @@ class ProductServiceTest {
                     // then
                     .isInstanceOf(IllegalArgumentException.class);
         }
+    }
+
+    @NotNull
+    private Menu createMenu(Product createProduct, MenuGroup menuGroup, String name, int price, boolean isDisplayed) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProduct(createProduct);
+        menuProduct.setQuantity(1);
+
+        Menu menu = new Menu();
+        menu.setId(UUID.randomUUID());
+        menu.setMenuGroup(menuGroup);
+        menu.setDisplayed(isDisplayed);
+        menu.setName(name);
+        menu.setPrice(BigDecimal.valueOf(price));
+        menu.setMenuProducts(List.of(menuProduct));
+        menuRepository.save(menu);
+        return menu;
+    }
+
+    @NotNull
+    private MenuGroup createMenuGroup(String name) {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(UUID.randomUUID());
+        menuGroup.setName(name);
+        menuGroupRepository.save(menuGroup);
+        return menuGroup;
     }
 }
